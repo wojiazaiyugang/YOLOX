e69f19cec7115b276de5b4728c212187cd14a27b
wojiazaiyugang <616986514@qq.com>
2021-09-09 01:10:36 +0800

    推理代码部分

--- tools/inference.py
+++ tools/inference.py
@@ -1,29 +1,169 @@
-import os
-from datetime import datetime
+import datetime
+from typing import Tuple
 
 import cv2
-from helper import onnx2tensorrt
+import torch
+import torchvision
+import numpy as np
+import pycuda.driver as cuda
+import pycuda.autoinit  # 不能删 init了啥玩意
+import tensorrt as trt
 
-from inference import do_inference
-from inference import get_engine, allocate_buffers, load_data
+TRT_LOGGER = trt.Logger()
+
+
+class HostDeviceMem(object):
+    def __init__(self, host_mem, device_mem):
+        self.host = host_mem
+        self.device = device_mem
+
+    def __str__(self):
+        return "Host:\n" + str(self.host) + "\nDevice:\n" + str(self.device)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+def allocate_buffers(engine):
+    inputs = []
+    outputs = []
+    bindings = []
+    stream = cuda.Stream()
+    for binding in engine:
+        size = trt.volume(engine.get_binding_shape(binding)) * engine.max_batch_size
+        dtype = trt.nptype(engine.get_binding_dtype(binding))
+        # Allocate host and device buffers
+        host_mem = cuda.pagelocked_empty(size, dtype)
+        device_mem = cuda.mem_alloc(host_mem.nbytes)
+        # Append the device buffer to device bindings.
+        bindings.append(int(device_mem))
+        # Append to the appropriate list.
+        if engine.binding_is_input(binding):
+            inputs.append(HostDeviceMem(host_mem, device_mem))
+        else:
+            outputs.append(HostDeviceMem(host_mem, device_mem))
+    return inputs, outputs, bindings, stream
+
+
+def do_inference(context, bindings, inputs, outputs, stream):
+    # Transfer input data to the GPU.
+    [cuda.memcpy_htod_async(inp.device, inp.host, stream) for inp in inputs]
+    # Run inference.
+    # context.execute_async_v2(bindings=bindings, stream_handle=stream.handle)
+    context.execute_async(bindings=bindings, stream_handle=stream.handle)
+    # Transfer predictions back from the GPU.
+    [cuda.memcpy_dtoh_async(out.host, out.device, stream) for out in outputs]
+    # Synchronize the stream
+    stream.synchronize()
+    # Return only the host outputs.
+    return [out.host for out in outputs]
+
+
+def pre_processing(img: np.ndarray, input_size: Tuple[int, int]):
+    """
+    图像预处理
+    :param input_size:
+    :param img:
+    :return:
+    """
+    padded_img = np.ones((input_size[0], input_size[1], 3), dtype=np.uint8) * 114
+    r = min(input_size[0] / img.shape[0], input_size[1] / img.shape[1])
+    resized_img = cv2.resize(
+        img,
+        (int(img.shape[1] * r), int(img.shape[0] * r)),
+        interpolation=cv2.INTER_LINEAR,
+    ).astype(np.uint8)
+    padded_img[: int(img.shape[0] * r), : int(img.shape[1] * r)] = resized_img
+    padded_img = padded_img.transpose((2, 0, 1))
+    padded_img = np.ascontiguousarray(padded_img, dtype=np.float32)
+    return padded_img
+
+
+def decode_outputs(outputs):
+    """
+    trt输出解码
+    :return:
+    """
+    grids = []
+    strides = []
+    dtype = torch.cuda.FloatTensor
+    for (hsize, wsize), stride in zip([torch.Size([80, 80]), torch.Size([40, 40]), torch.Size([20, 20])], [8, 16, 32]):
+        yv, xv = torch.meshgrid([torch.arange(hsize), torch.arange(wsize)])
+        grid = torch.stack((xv, yv), 2).view(1, -1, 2)
+        grids.append(grid)
+        shape = grid.shape[:2]
+        strides.append(torch.full((*shape, 1), stride))
+    grids = torch.cat(grids, dim=1).type(dtype)
+    strides = torch.cat(strides, dim=1).type(dtype)
+    outputs[..., :2] = (outputs[..., :2] + grids) * strides
+    outputs[..., 2:4] = torch.exp(outputs[..., 2:4]) * strides
+    return outputs
+
+
+def post_processing(prediction, num_classes, conf_thre=0.7, nms_thre=0.45, class_agnostic=False):
+    box_corner = prediction.new(prediction.shape)
+    box_corner[:, :, 0] = prediction[:, :, 0] - prediction[:, :, 2] / 2
+    box_corner[:, :, 1] = prediction[:, :, 1] - prediction[:, :, 3] / 2
+    box_corner[:, :, 2] = prediction[:, :, 0] + prediction[:, :, 2] / 2
+    box_corner[:, :, 3] = prediction[:, :, 1] + prediction[:, :, 3] / 2
+    prediction[:, :, :4] = box_corner[:, :, :4]
+    output = [None for _ in range(len(prediction))]
+    for i, image_pred in enumerate(prediction):
+
+        # If none are remaining => process next image
+        if not image_pred.size(0):
+            continue
+        # Get score and class with highest confidence
+        class_conf, class_pred = torch.max(image_pred[:, 5: 5 + num_classes], 1, keepdim=True)
+        conf_mask = (image_pred[:, 4] * class_conf.squeeze() >= conf_thre).squeeze()
+        # Detections ordered as (x1, y1, x2, y2, obj_conf, class_conf, class_pred)
+        detections = torch.cat((image_pred[:, :5], class_conf, class_pred.float()), 1)
+        detections = detections[conf_mask]
+        if not detections.size(0):
+            continue
+        if class_agnostic:
+            nms_out_index = torchvision.ops.nms(
+                detections[:, :4],
+                detections[:, 4] * detections[:, 5],
+                nms_thre,
+            )
+        else:
+            nms_out_index = torchvision.ops.batched_nms(
+                detections[:, :4],
+                detections[:, 4] * detections[:, 5],
+                detections[:, 6],
+                nms_thre,
+            )
+        detections = detections[nms_out_index]
+        if output[i] is None:
+            output[i] = detections
+        else:
+            output[i] = torch.cat((output[i], detections))
+    return output
+
 
 if __name__ == '__main__':
-    # test()
-    # exit()
-
-    onnx_file = r"/home/senseport0/Workspace/HiAlgorithm/mmdetection/checkpoints/ssd_openpai_epoch1.onnx"
-    trt_file = r"/home/senseport0/Workspace/HiAlgorithm/mmdetection/checkpoints/ssd_openpai_epoch1.trt"
-    # build_engine(onnx_file, trt_file)
-    # onnx2tensorrt(onnx_file, trt_file)
-    # exit()
-
-    # test_dir = r"/home/senseport0/Workspace/HiAlgorithm/mmclassification/data/goal_classification/test"
-    # d = os.path.join(test_dir, "0")
-    with get_engine(trt_file) as engine, engine.create_execution_context() as context:
-        inputs, outputs, bindings, stream = allocate_buffers(engine=engine)
+    engine_file_path = "/home/senseport0/Workspace/YOLOX/YOLOX_outputs/yolox_l_basketball_detection/model_trt.engine"
+    input_image = cv2.imread('/home/senseport0/Workspace/YOLOX/assets/1808.jpg')
+    input_size = (640, 640)  # 输入图像尺寸
+    with open(engine_file_path, "rb") as f, trt.Runtime(TRT_LOGGER) as runtime, runtime.deserialize_cuda_engine(
+            f.read()) as engine, engine.create_execution_context() as context:
+        # cfx = cuda.Device(0).make_context()
+        inputs, outputs, bindings, stream = allocate_buffers(engine)
         for i in range(100):
-            s = datetime.now()
-            inputs[0].host = load_data(cv2.imread(os.path.join(r"./test_image.jpg")))
-            output = do_inference(context=context, bindings=bindings, inputs=inputs, outputs=outputs, stream=stream)
-            print((datetime.now() - s).total_seconds() * 1000)
-        print(output)
+            s = datetime.datetime.now()
+            image = pre_processing(input_image, input_size)
+            np.copyto(inputs[0].host, image.flatten())
+
+            # cfx.push()
+            trt_outputs = do_inference(context, bindings=bindings, inputs=inputs, outputs=outputs, stream=stream)
+            # cfx.pop()
+            # trt_outputs = torch.from_numpy(trt_outputs[0]).resize(1, 8400, 6).cuda()
+            trt_outputs = decode_outputs(trt_outputs)
+            # trt_outputs = post_processing(prediction=trt_outputs,
+            #                               num_classes=1,
+            #                               conf_thre=0.3,
+            #                               nms_thre=0.3,
+            #                               class_agnostic=True)
+            # print(trt_outputs[0].cpu().numpy())
+            print((datetime.datetime.now() - s).total_seconds() * 1000)
