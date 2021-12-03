b540edf49df636a7ad4cd1dcaa6ef482bb75374e
wojiazaiyugang <13061044@buaa.edu.cn>
2021-12-03 17:22:59 +0800

    拳套检测修改num class

--- exps/boxing_gloves/boxing_gloves_yolox_l.py
+++ exps/boxing_gloves/boxing_gloves_yolox_l.py
@@ -1,7 +1,11 @@
+import os
+
 from exps.basketball.basketball_yolox_l import Exp as MyExp
 
 
 class Exp(MyExp):
     def __init__(self):
         super().__init__()
+        self.num_classes = 2
+        self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
         self.basketball_detection_dir = "2022"
