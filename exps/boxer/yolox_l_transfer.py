ac1926280815c5e5f02b2addacda0c3774c64b11
wojiazaiyugang <wojiazaiyugang@outlook.com>
2022-04-22 21:58:26 +0800

    拳手检测改为两个类别，直接检测红蓝双方

--- exps/boxer/yolox_l_transfer.py
+++ exps/boxer/yolox_l_transfer.py
@@ -4,7 +4,7 @@ from exps.boxing_gloves.yolox_l import Exp as MyExp
 class Exp(MyExp):
     def __init__(self):
         super().__init__()
-        self.num_classes = 1
+        self.num_classes = 2
         self.exp_name = "human_transfer"
         self.voc_dir_suffix = "2023"
 
