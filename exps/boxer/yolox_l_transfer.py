26277f13efa971fe22cac565a61256f36d8a235d
wojiazaiyugang <wojiazaiyugang@outlook.com>
2022-04-24 20:48:16 +0800

    更新拳手检测迁移学习配置文件

--- exps/boxer/yolox_l_transfer.py
+++ exps/boxer/yolox_l_transfer.py
@@ -1,12 +1,7 @@
-from exps.boxing_gloves.yolox_l import Exp as MyExp
+from exps.boxer.yolox_l import Exp as MyExp
 
 
 class Exp(MyExp):
-    def __init__(self):
-        super().__init__()
-        self.num_classes = 2
-        self.exp_name = "boxer_detection_transfer"
-        self.voc_dir_suffix = "2023"
 
     def get_model(self):
         from yolox.utils import freeze_module
