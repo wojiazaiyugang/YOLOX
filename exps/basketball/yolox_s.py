2cc2814a0008210f60a2b7508d25e381987d4a09
wojiazaiyugang <13061044@buaa.edu.cn>
2022-05-16 09:23:40 +0800

    篮球检测exp添加name

--- exps/basketball/yolox_s.py
+++ exps/basketball/yolox_s.py
@@ -4,5 +4,7 @@ from exps.basketball.yolox_l import Exp as MyExp
 class Exp(MyExp):
     def __init__(self):
         super().__init__()
+        self.exp_name = "basketball_yolox_l"
         self.depth = 0.33
         self.width = 0.50
+
